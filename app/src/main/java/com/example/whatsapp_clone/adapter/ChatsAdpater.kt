d25125670b0828a26802c6db0effe5b21bfa12fd
dhandyjoe <dhanjoenkp@gmail.com>
2021-09-15 23:06:20 +0700

    update app

---
+++ app/src/main/java/com/example/whatsapp_clone/adapter/ChatsAdpater.kt
@@ -0,0 +1,18 @@
+package com.example.whatsapp_clone.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatsAdpater(val data: ArrayList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private class MyViewHolder(binding: )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = data.size
+}
